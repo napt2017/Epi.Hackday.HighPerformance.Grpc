0a8ee4f13431e5e5022cd8c2c92d62497ccb39e0
napt2010 <ngocanhk6apro@gmail.com>
2020-11-16 17:08:28 +0700

    Change to WPFclient

---
+++ WPF.Client/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitGridLayout();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InitGridLayout()
+        {
+            mainGridLayout.ShowGridLines = true;
+            mainGridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            mainGridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+
+            mainGridLayout.RowDefinitions.Add(new RowDefinition());
+            mainGridLayout.RowDefinitions.Add(new RowDefinition());
+
+            var rpcComponent = new RPCComponent();
+            mainGridLayout.Children.Add(rpcComponent);
+            Grid.SetRow(rpcComponent, 0);
+            Grid.SetColumn(rpcComponent, 0);
+
+            rpcComponent = new RPCComponent();
+            mainGridLayout.Children.Add(rpcComponent);
+            Grid.SetRow(rpcComponent, 0);
+            Grid.SetColumn(rpcComponent, 1);
+
+            rpcComponent = new RPCComponent();
+            mainGridLayout.Children.Add(rpcComponent);
+            Grid.SetRow(rpcComponent, 1);
+            Grid.SetColumn(rpcComponent, 0);
+
+            rpcComponent = new RPCComponent();
+            mainGridLayout.Children.Add(rpcComponent);
+            Grid.SetRow(rpcComponent, 1);
+            Grid.SetColumn(rpcComponent, 1);
+        }
+    }
+}
---
+++ WPF.Client/RPCComponent.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.Client
+{
+    /// <summary>
+    /// Interaction logic for RPCComponent.xaml
+    /// </summary>
+    public partial class RPCComponent : UserControl
+    {
+        public RPCComponent()
+        {
+            InitializeComponent();
+        }
+    }
+}
